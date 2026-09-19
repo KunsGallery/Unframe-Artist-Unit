db0847cf98dfd6c18165e12e8b55d9a758740808
KunsGallery <gallerykuns@gmail.com>
2026-09-19 17:46:25 +0900

    Add inline admin approval requests

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,5 +3,5 @@ import AdminTestingPanel from "./testing-panel";
 import AdminEditorTab from "./editor-tab";
 
 export default function AdminPage() {
-  return <><AdminClient /><AdminEditorTab /><AdminTestingPanel /><a className="admin-route-link" href="/admin/access">Role access review ↗</a></>;
+  return <><AdminClient /><AdminEditorTab /><AdminTestingPanel /></>;
 }
